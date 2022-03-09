72b73a62071c48a692ce15bffc65f4edf2f3f863
Barbara <barbara.lopez@ikasle.egibide.org>
2022-03-09 10:31:36 +0100

    mejoras

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -49,28 +49,3 @@
 </div>
 
 @endsection
-<script>
-    function compra(id){
-        var productos = sessionStorage.getItem('carrito');
-        if(productos){//Si hay mas productos
-            var productosFind=[];
-            productosFind=productos.split(" ");
-            var duplicado=false;
-            productosFind.find(function(element){//Busca si el producto ya existe
-                if(element==id){
-                    duplicado=true;
-                }
-            });
-            if(duplicado==true){
-                console.log("El producto esta duplicado");
-            }else{
-                productos=productos + " " + id;
-                sessionStorage.setItem('carrito',productos);
-            }
-        }else{//Si el carrito esta vacio
-            productos=id;
-            sessionStorage.setItem('carrito',productos);
-        }
-      
-    }
-</script>
